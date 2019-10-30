996613ff60b1b6c89696b433d1536d020eef48cd
devremoto2 <adilson@almeidapedro.com.br>
2019-10-30 08:09:11 -0300

    ng update  to version 8.2.12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 
@@ -14,7 +13,7 @@ import { TodosComponent } from './todos/todos.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
